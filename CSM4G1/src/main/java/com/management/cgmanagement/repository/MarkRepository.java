1973c3e17fd77ae0222b27702ebad1eefcd5004d
NguyenThanhVinh2711 <vinh99657@gmail.com>
2022-12-23 11:23:09 +0700

    done

---
+++ CSM4G1/src/main/java/com/management/cgmanagement/repository/MarkRepository.java
@@ -0,0 +1,19 @@
+package com.management.cgmanagement.repository;
+
+import com.management.cgmanagement.model.dto.IMark;
+import com.management.cgmanagement.model.dto.IStudent;
+import com.management.cgmanagement.model.entity.Course;
+import com.management.cgmanagement.model.entity.Mark;
+import com.management.cgmanagement.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+
+public interface MarkRepository extends JpaRepository<Mark,Long> {
+    @Query(nativeQuery = true ,value = "select full_name,lecture,tutorial,gpa from users join mark on `users`.`id` = `mark`.`user_id`;")
+    Iterable<IMark> getMarkNative();
+
+    @Query(nativeQuery = true,value = "select `users`.`full_name`,`users`.`date_of_birth`,`users`.`address`,`users`.`email`,`users`.`phone_number`,`classes`.`name_class` from users join user_roles on `users`.`id` = `user_roles`.`role_id` join user_class on `users`.`id` = `user_class`.`user_id` join classes on `user_class`.`class_room_id` = `classes`.`id`  ;")
+    Iterable<IStudent> getStudent();
+}
---
+++ CSM4G1/src/main/java/com/management/cgmanagement/service/mark/IMarkService.java
@@ -0,0 +1,18 @@
+package com.management.cgmanagement.service.mark;
+
+import com.management.cgmanagement.model.dto.IMark;
+import com.management.cgmanagement.model.dto.IStudent;
+import com.management.cgmanagement.model.entity.Mark;
+import com.management.cgmanagement.service.GenericService;
+
+import java.util.Optional;
+
+public interface IMarkService  {
+//    Mark saveMark(Double lecture, Double tutorial, Long course_id, Long user_id);
+    Iterable<IMark> getMarkNative();
+    Iterable<IStudent> getStudent();
+    Optional<Mark> findById(Long id);
+    Mark save(Mark mark );
+    void remove(Long id);
+
+}
